07a0a2bd6bab75c9e2ffaed203a61675019ef9ad
Alex Cosmin <alex619axl@gmail.com>
2024-01-28 13:58:03 +0200

    Update crack.py

--- crack.py
+++ crack.py
@@ -2,6 +2,7 @@ import sys
 import threading
 import bcrypt
 from tqdm import tqdm
+import os
 
 # Variables for the total number of attempted passwords and to check if the password has been found
 total_passwords_attempted = 0
@@ -34,13 +35,21 @@ logo()
 
 #--------------------------------------------------------
 hash_input = input("Hash > ")  # Example: $P$FDj9XhuS43ooqUPB4EVddWAT5lCWyA1
-passl = "list.txt"  # Change it to the name of the wordlist
+
+# Ask the user for the path to the wordlist
+passl = input("Enter the path to the wordlist: ")
+
+# Check if the provided file exists
+if not os.path.exists(passl):
+    print(bcolors.FAIL + "| Couldn't find the required list!")
+    sys.exit(1)
+
 threads = 1
 
 try:
     plist = open(passl).readlines()
 except:
-    print(bcolors.FAIL + "| Couldn't find the required list!")
+    print(bcolors.FAIL + "| Couldn't read the provided wordlist!")
 
 #--------------------------------------------------------
 def crack(password):
@@ -63,6 +72,10 @@ def crack(password):
 
         total_passwords_attempted += 1
         tqdm.write("Progress: {:,} passwords attempted".format(total_passwords_attempted), end='\r')
+
+    # If the password has not been found and all passwords from the list have been attempted, display an error message
+    if total_passwords_attempted == len(plist):
+        print(bcolors.FAIL + "Password not found. Try with another wordlist.")
 
 #--------------------------------------------------------
 def display_password_details(password):
